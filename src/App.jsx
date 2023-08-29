757f6c060a8435cc4b063a30f9b83cc9b7208209
javierJeldes <jeldesldc@gmail.com>
2023-08-29 00:21:48 -0400

    avance autentificacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import "./style.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./Context/AuthContext";
 
 function App() {
+
+  const {currentUser} = useContext(AuthContext);
+  console.log(currentUser);
+
   return (
       <BrowserRouter>
       <Routes>
         <Route path="/">
-        <Route index element={<Home/>}></Route>
-        <Route path="login" element= {<Login/>}></Route>
-        <Route path="Register" element= {<Register/>}></Route>
+        <Route index element={<Home/>}/>
+        <Route path="login" element= {<Login/>}/>
+        <Route path="Register" element= {<Register/>}/>
         </Route>
       </Routes>
       </BrowserRouter>
