4609f1a90ab62b1a253ab2a389a492fe3cb92ad8
javierJeldes <jeldesldc@gmail.com>
2023-09-03 04:00:08 -0300

    creacion de chats, falta modificar userchats - no se ingresan

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import {collection,query,where,getDocs} from "firebase/firestore";
+import React, { useContext, useState } from 'react'
+import {collection,query,where,getDocs, getDoc, setDoc, doc, updateDoc, serverTimestamp} from "firebase/firestore";
 import {db} from "../Firebase";
+import { AuthContext } from '../Context/AuthContext';
 
 const SearchBar = () => {
 
@@ -8,45 +9,107 @@ const SearchBar = () => {
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false); 
 
+  const {currentUser} = useContext(AuthContext);
+
   const handleSearch = async ()=>{
+    
     const q = query(
       collection(db, "users"),
-      where("displayName","==",username)
-    );
+      where("displayName","==",username));
+      
     try{
       const querySnapshot = await getDocs(q);
+    
       querySnapshot.forEach((doc) => {
         setUser(doc.data())
-        console.log("asdasd");
       });
 
     }catch(err){
+      console.log(err);
       setErr(true);
     }
   };
 
   const handleKey = (e) =>{
-    console.log(e);
-    e.code === "Enter" && handleSearch();
+    e.code === "Enter" && handleSearch(); 
+  };
+
+  const handleSelect = async () =>{
+
+    
+    const combinedId = currentUser.uid > user.uid 
+    ? currentUser.uid + user.uid 
+    : user.uid + currentUser.uid;
+    
+    try{
+      const res = await getDoc(doc(db,"chats",combinedId));
+      
+      if(!res.exists()){
+        await setDoc(doc(db,"chats",combinedId),{messages:[]});
+
+        await updateDoc(doc(db,"userChats", currentUser.uid),{
+        [combinedId+".userInfo"]: {
+          uid:user.uid,
+          displayName:user.displayName,
+          photoURL:user.photoURL
+        },
+        [combinedId+".date"]:serverTimestamp(),
+        });
+
+        console.log("error currentUser")
+        await updateDoc(doc(db,"userChats", user.uid),{
+          [combinedId+".userInfo"]: {
+            uid:currentUser.uid,
+            displayName:currentUser.displayName,
+            photoURL:currentUser.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp(),
+          });
+
+          console.log("error user")
+      }
+    }catch(err){console.log("ddd",err)}  
   };
 
   return (
     <div className='searchbar'> 
       <div className='searchForm'>
-        <input type='text' onKeyDown={handleKey} onChange={e=>setUsername} placeholder='Encontrar Usuario'/>
+        <input type='text' onKeyDown={handleKey} onChange={(e)=>setUsername(e.target.value)} value={username} placeholder='Encontrar Usuario'/>
       </div>
-
-      {err && <span>User not Found</span>}
-      {user && <div className='userChat'>
-      
-        <img src={user.photoURL} alt=''/>
-        <div className="userChatInfo">
-          <span>{user.displayName}</span>
-        </div>
-
-      </div>}
+      <SearchUserInfo user={user} handleSelect={handleSelect}></SearchUserInfo>
     </div>
   )
 }
+
+const SearchUserInfo = ({user, handleSelect}) => {
+
+  if(user){
+    return(
+      <div className='userChat' onClick={handleSelect}>
+          <img src={user.photoURL} alt=''/>
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+      </div>
+    )   
+  }
+}
+
+// const SearchUserInfoEmpty = () => {
+//   return(
+//     <div className='userChat'>
+//           <span>fgfffffff</span>
+//           <div className="userChatInfo">
+//             <span>dasdasdsa</span>
+//           </div>
+//       </div>
+//   )
+  
+// }
+
+
+
+
+
 
 export default SearchBar
